895f46e0e8439a4b4bf46c7973ebd34c75d5ed16
Andrikha11 <andrikharamadhon@student.uir.ac.id>
2023-05-20 17:01:56 +0700

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# vue-router-ridho
+# vue-router-andrikha
+
+Add stuff
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
    
     <div class="wrapper">
-      <HelloWorld msg="Vue Router - RIDHO!" />
+      <HelloWorld msg="Vue Router - ANDRIKHA!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
